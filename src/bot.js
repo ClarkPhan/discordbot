cf3991899083b57067a9709d33dfca2149210151
Clark Phan <clarkphan24@gmail.com>
2021-05-16 19:47:05 -0700

    Stock Data Card Prototype

--- src/bot.js
+++ src/bot.js
@@ -1,30 +1,31 @@
 require('dotenv').config();
 
 const stock = require('./stock.js');
-const { Client } = require('discord.js');
+const { Client, MessageAttachment } = require('discord.js');
 const client = new Client();
 const PREFIX = '!';
+const MAX_ARGS = 2;
 
 client.on('ready', () => {
 	console.log(`${client.user.tag} has logged in.`);
-})
+});
 
 client.on('message', (message) => {
-	if (message.author.bot) return;
-	if (message.content.startsWith(PREFIX)) {
-		const [CMD_NAME, ...args] = message.content
-			.trim()
-			.substring(PREFIX.length)
-			.split(/\s+/);
-		if (CMD_NAME === 'stock') {
-			if (args.length === 0) {
-				return message.reply('Please provide stock id, pepelaugh.');
-			}
-			args.forEach(symbol => {
-				stock.fetchStock(message, symbol);
-			})
+	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+	const [CMD_NAME, ...args] = message.content
+		.trim()
+		.substring(PREFIX.length)
+		.split(/\s+/);
+	if (CMD_NAME === 'stock') {
+		if (args.length === 0) {
+			return message.reply('Please provide stock id, pepelaugh.');
+		} else if (args.length > MAX_ARGS) {
+			const attachment = new MessageAttachment('./media/kekbomb.gif');
+			message.reply(`*Pepelaugh OH NO NO NO* I can only accept up to **2** stock symbols!`, attachment);
+		} else {
+			args.forEach(symbol => stock.fetchStock(message, symbol));
 		}
 	}
-})
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
--- src/stock.js
+++ src/stock.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
+const { MessageAttachment, MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
+const clearbit = require('clearbit')(process.env.CLEARBIT_API_KEY);
 const plotly = require('plotly')(process.env.PLOTLY_USERNAME, process.env.PLOTLY_API_KEY);
+const up = ':chart_with_upwards_trend:';
+const down = ':chart_with_downwards_trend:';
 
 function fetchStock(message, symbol) {
 	const query = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE'
@@ -9,35 +13,86 @@ function fetchStock(message, symbol) {
 		+ `&apikey=${process.env.STOCK_API_KEY}`;
 	fetch(query)
 		.then(response => response.json())
-		.then(data => getStockQuote(message, symbol, data));
+		.then(data => processStockData(message, symbol, data));
+}
+function getStockLogo(message, symbol, data, overview) {
+	if (overview === undefined || overview === null || Object.keys(overview).length === 0) {
+		return generateStockEmbedMessage(message, symbol, data);
+	}
+	console.log(overview['Name']);
+	let name = overview['Name'].split(',');
+	name = name[0].replace('Corporation', '').replace('Corp', '').replace('Inc', '').replace('Global', '').replace('.com', '');
+	console.log(name);
+	const NameToDomain = clearbit.NameToDomain;
+	NameToDomain.find({ name })
+		.then(logoResult => generateStockEmbedMessage(message, symbol, data, overview, logoResult))
+		.catch(NameToDomain.NotFoundError, err => console.log(err))
+		.catch(() => console.log('Bad/invalid request, unauthorized, Clearbit error, or failed request'));
 }
 
-function getStockQuote(message, symbol, data) {
-	if (Object.keys(data['Global Quote']).length === 0) {
-		message.reply('Invalid Stock Name, pepelaugh.');
-		return;
-	}
-
+function generateStockEmbedMessage(message, symbol, data, overview, logoResult) {
 	const quote = data['Global Quote'];
 	const price = parseFloat(quote['05. price']);
 	const open = parseFloat(quote['02. open']);
 	const high = parseFloat(quote['03. high']);
 	const low = parseFloat(quote['04. low']);
 	const change = parseFloat(quote['09. change']);
-	const changePecentage = parseFloat(quote['10. change percent']);
-
-	message.reply(
-		`*Pepelaugh*. \n**$${symbol.toUpperCase()}**\n`
-		+ `Price: ${price}\n`
-		+ `Change: ${change}\n`
-		+ `Change Percent: ${changePecentage}\n\n`
-		+ `Open: ${open}\n`
-		+ `High: ${high}\n`
-		+ `Low: ${low}\n`
-
-	);
+	const changePercent = parseFloat(quote['10. change percent']);
+	const changes = formatPriceChangeText(change, changePercent);
+	let name;
+	let thumbnail;
+	if (overview === undefined) {
+		name = symbol;
+	} else {
+		name = overview['Name'];
+	}
+	if (logoResult === undefined) {
+		thumbnail = null;
+	} else {
+		thumbnail = logoResult.logo;
+	}
+	const stockEmbed = new MessageEmbed()
+		.setColor('GOLD')
+		.setTitle(`**$${symbol.toUpperCase()}**`)
+		.setAuthor(name)
+		// .setDescription(overview['Description'])
+		.addFields(
+			{ name: 'Price', value: `$${price} ${change > 0 ? up : down}` },
+			{ name: 'Changes', value: changes },
+			{ name: 'Open', value: `$${open}`, inline: true },
+			{ name: 'High', value: `$${high}`, inline: true },
+		)
+		.addField('Low', `$${low}`, true)
+		.setTimestamp()
+		.setFooter(`© Clark Phan ${thumbnail !== null ? '(logos provided by Clearbit API)' : ''} `, 'https://clarkphan.com/assets/images/me.JPG');
+	if (thumbnail) {
+		stockEmbed.setThumbnail(thumbnail);
+	}
+	message.reply(stockEmbed);
+}
+function processStockData(message, symbol, data) {
+	if (Object.keys(data['Global Quote']).length === 0) {
+		const attachment = new MessageAttachment('./media/monkaX.gif');
+		message.reply(`__**${symbol}**__ is an invalid stock symbol!`, attachment);
+		return;
+	}
+	const query = 'https://www.alphavantage.co/query?function=OVERVIEW'
+		+ `&symbol=${symbol}`
+		+ `&apikey=${process.env.STOCK_API_KEY}`;
+	fetch(query)
+		.then(response => response.json())
+		.then(overview => getStockLogo(message, symbol, data, overview));
 }
 
+function formatPriceChangeText(change, changePercent) {
+	if (change > 0) {
+		return '```diff\n' + `+${change} +${changePercent}%` + '```';
+	}
+	return '```diff\n' + `${change} ${changePercent}%` + '```';
+}
+
+
+// eslint-disable-next-line no-unused-vars
 function generateStockGraph(message, symbol, data) {
 	if (data['Error Message']) {
 		message.reply('Invalid Stock Name, pepelaugh.');
@@ -49,10 +104,10 @@ function generateStockGraph(message, symbol, data) {
 		y: [],
 		type: 'scatter',
 		mode: 'lines',
-		line: {color: '#17BECF'}
-	}
+		line: { color: '#17BECF' }
+	};
 	const points = data['Time Series (1min)'];
-	for (point in points) {
+	for (const point in points) {
 		stockData.x.push(point);
 		stockData.y.push(points[point]['1. open']);
 	}
@@ -61,8 +116,8 @@ function generateStockGraph(message, symbol, data) {
 		filename: `${symbol}`,
 		yaxis: {
 			autorange: true,
-			type: 'linear',
-		}
+			type: 'linear'
+		},
 	};
 	plotly.plot(stockData, layout, (err, response) => {
 		if (err) return console.log(err);
@@ -70,5 +125,4 @@ function generateStockGraph(message, symbol, data) {
 	});
 }
 
-module.exports = { fetchStock }
-
+module.exports = { fetchStock };
